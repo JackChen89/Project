ecfa213a2722c91ffb10de4bf730f9ab2cbfc763
grace890306 <46022284+grace890306@users.noreply.github.com>
2019-01-06 19:32:51 +0800

    新增兩首歌

--- 0105/太鼓達人/record.py
+++ 0105/太鼓達人/record.py
@@ -38,7 +38,7 @@ def drawText(text, font, surface, x, y):        # 繪製文字到視窗中
     surface.blit(textobj,textrect)
 
 mixer.init()                                    # 建立 Soumd 物件
-mixer.music.load('C:\\Users\\USER\\Desktop\\Jack\\107-1course\\PBC\\Project\\final\\drum.mp3')             # 設定背景音樂
+mixer.music.load('C:\\Users\\USER\\Desktop\\drum.mp3')             # 設定背景音樂
 mixer.music.play()
 
 pygame.init()                                   # 初始化 pygame
@@ -47,7 +47,7 @@ font2 = pygame.font.SysFont(None,30)            # None:使用系統預字型
 screen = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT+40))    
 pygame.display.set_caption("太鼓達人")          # 設定視窗標題
 screen.fill(BACKGROUNDCOLOR) 
-image = pygame.image.load('C:\\Users\\USER\\Desktop\\Jack\\107-1course\\PBC\\Project\\final\\logo_taiko.png')     # 載入圖片
+image = pygame.image.load('C:\\Users\\USER\\Desktop\\logo_taiko.png')     # 載入圖片
 screen.blit(image,(0,0))                        # 繪製圖片
 drawText('Press a key to start.', font, screen, 400,600)
 pygame.display.update()
@@ -82,29 +82,29 @@ while Running:
     song = song.lower()
     l = len(song)
     songstr = song[0:l-1]                      #字串處理(因textbox多一個\n，並變成小寫)
-    mixer.music.load('C:\\Users\\USER\\Desktop\\Jack\\107-1course\\PBC\\Project\\final\\music\\%s.mp3'%songstr) # 載入背景音樂
+    mixer.music.load('C:\\Users\\USER\\Desktop\\music\\%s.mp3'%songstr) # 載入背景音樂
     mixer.music.play()
     
     background = pygame.Surface(screen.get_size())  # 建立畫布
     background.fill(BACKGROUNDCOLOR)                # 顯示畫布為白色
-    image = pygame.image.load('C:\\Users\\USER\\Desktop\\Jack\\107-1course\\PBC\\Project\\final\\drum_Basemap.png')   # 載入圖片
+    image = pygame.image.load('C:\\Users\\USER\\Desktop\\drum_Basemap.png')   # 載入圖片
     background.blit(image,(0,0))                    # 繪製圖片
                                 
     new_drums1 = []
     new_drums2 = []
  
-    image = pygame.image.load("C:\\Users\\USER\\Desktop\\Jack\\107-1course\\PBC\\Project\\final\\drum1.png")   # 載入背景圖片
+    image = pygame.image.load("C:\\Users\\USER\\Desktop\\drum1.png")   # 載入背景圖片
     drumr_image = pygame.transform.scale(image, (50,50))             # 重新設定鼓的大小(紅)
 
-    image = pygame.image.load("C:\\Users\\USER\\Desktop\\Jack\\107-1course\\PBC\\Project\\final\\drum2.png")   # 載入背景圖片
+    image = pygame.image.load("C:\\Users\\USER\\Desktop\\drum2.png")   # 載入背景圖片
     drumb_image = pygame.transform.scale(image, (50,50))             # 重新設定鼓的大小(藍)
 
     good = pygame.Rect(335,190,100,100)                                  # 存放 Rect 物件，記錄good的位置及大小    
-    image = pygame.image.load("C:\\Users\\USER\\Desktop\\Jack\\107-1course\\PBC\\Project\\final\\good.png")    # 載入背景圖片
+    image = pygame.image.load("C:\\Users\\USER\\Desktop\\good.png")    # 載入背景圖片
     good_image = pygame.transform.scale(image, (100,100))               # 重新設定good的大小  
     
     bad = pygame.Rect(360,190,100,100)                                  # 存放 Rect 物件，記錄bad的位置及大小    
-    image = pygame.image.load("C:\\Users\\USER\\Desktop\\Jack\\107-1course\\PBC\\Project\\final\\bad.png")     # 載入背景圖片
+    image = pygame.image.load("C:\\Users\\USER\\Desktop\\bad.png")     # 載入背景圖片
     bad_image = pygame.transform.scale(image, (50,30))                  # 重新設定bad的大小    
     # print(drums)
       
@@ -139,13 +139,13 @@ while Running:
         for i in range(len(new_drums1)):#紅鼓的運行及打擊            
             # new_drums1[i] -= dx                                    
             if pressr == 1:
-              if (abs(min(new_drums1) - 375)) <= 25:#若按下且在範圍內
+              if (abs(min(new_drums1) - 375)) <= 35:#若按下且在範圍內
                 # new_drums1[new_drums1.index(min(new_drums1))] = max
                 score += 5 * (1 + combo / 100)#加上combo的計分，越多越高分  
                 screen.blit(good_image, good) 
                 pressr = 0
                 combo += 1
-              elif (abs(min(new_drums1) - 375)) >= 25:#若按下但不在範圍內
+              elif (abs(min(new_drums1) - 375)) >= 35:#若按下但不在範圍內
                 screen.blit(bad_image, bad) 
                 pressr = 0
                 combo = 0
@@ -158,13 +158,13 @@ while Running:
         for r in range(len(new_drums2)):#藍鼓的運行及打擊
             # new_drums2[r] -= dx 
             if pressb == 1:
-              if (abs(min(new_drums2) - 375)) <= 25:
+              if (abs(min(new_drums2) - 375)) <= 35:
                 # new_drums2[new_drums2.index(min(new_drums2))] = max
                 score += 3 * (1 + combo / 100)  
                 screen.blit(good_image, good) 
                 pressb = 0
                 combo += 1
-              elif (abs(min(new_drums2) - 375)) >= 25:
+              elif (abs(min(new_drums2) - 375)) >= 35:
                 screen.blit(bad_image, bad) 
                 pressb = 0
                 combo = 0
@@ -188,7 +188,7 @@ while Running:
    
     # 遊戲結束，停止播放背景音樂，顯示分數
     pygame.mixer.music.stop()
-    mixer.music.load('C:\\Users\\USER\\Desktop\\Jack\\107-1course\\PBC\\Project\\final\\gameover.mp3')
+    mixer.music.load('C:\\Users\\USER\\Desktop\\gameover.mp3')
     mixer.music.play()
     print("!!!!!It's the data of",songstr,"!!!!!")
     print("Red:",new_drums1)
@@ -196,7 +196,7 @@ while Running:
 
     screen = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT+40)) 
     screen.fill(BACKGROUNDCOLOR) 
-    image = pygame.image.load("C:\\Users\\USER\\Desktop\\Jack\\107-1course\\PBC\\Project\\final\\score_Basemap.png")  # 載入圖片
+    image = pygame.image.load("C:\\Users\\USER\\Desktop\\score_Basemap.png")  # 載入圖片
     background.blit(image,(0,0))                    # 繪製圖片
     screen.blit(background,(0,0))                   # 重繪視窗
     
